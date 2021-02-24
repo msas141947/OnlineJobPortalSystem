098d1fb871ceb2722264862f756f98b7f902370d
shafait <msas141947@gmail.com>
2021-02-24 13:19:06 +0500

    first commit

---
+++ job/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class StudentUser(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=15,null=True)
+    image = models.FileField(null=True)
+    gender = models.CharField(max_length=10,null=True)
+    type = models.CharField(max_length=15,null=True)
+    def _str_(self):
+        return self.user.username
+
+
+
+
+class Recruiter(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    mobile = models.CharField(max_length=15,null=True)
+    image = models.FileField(null=True)
+    gender = models.CharField(max_length=10,null=True)
+    company = models.CharField(max_length=50,null=True)
+    type = models.CharField(max_length=15,null=True)
+    status = models.CharField(max_length=20, null=True)
+    def _str_(self):
+        return self.user.username
+
+class Job(models.Model):
+    recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    title = models.CharField(max_length=50)
+    salary = models.FloatField(max_length=10, null=True)
+    image = models.FileField()
+    description = models.CharField(max_length=200)
+    experience = models.CharField(max_length=20)
+    location = models.CharField(max_length=50)
+    skills = models.CharField(max_length=100)
+    creationdate = models.DateField()
+
+    def _str_(self):
+         return self.title
+
+
